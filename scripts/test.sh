f93dc232522f4354f26a3e56d3cf59ba1fcafbe8
Gian Calgeer <gidoca@gmail.com>
2024-10-18 20:39:17 +0200

    Use CMake

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,19 +2,20 @@ set -e
 
 SCRIPTDIR=$(dirname "$0")
 BASEDIR="${SCRIPTDIR}/.."
+BUILDDIR="${BASEDIR}/build"
 SRCDIR="${BASEDIR}/src"
-BINDIR="${BASEDIR}/bin"
+BINDIR="${BUILDDIR}/src"
 TESTDIR="${BASEDIR}/testdata"
 CURRENTTESTDIR="${TESTDIR}/current"
 
-clang++ -std=c++20 -stdlib=libc++ -Wall "${SRCDIR}/write_bmp.cpp" -o ${BINDIR}/write_bmp
+cmake -B "${BUILDDIR}" .
+cmake --build "${BUILDDIR}"
+
 ${BINDIR}/write_bmp "${CURRENTTESTDIR}/test.bmp"
 diff "${CURRENTTESTDIR}/test.bmp" "${TESTDIR}/test.bmp"
 
-clang++ -std=c++20 -stdlib=libc++ -Wall "${SRCDIR}/mandelbrot.cpp" -o ${BINDIR}/write_mandelbrot
 ${BINDIR}/write_mandelbrot "${CURRENTTESTDIR}/mandelbrot.bmp"
 diff "${CURRENTTESTDIR}/mandelbrot.bmp" "${TESTDIR}/mandelbrot.bmp"
 
-clang++ -std=c++20 -stdlib=libc++ -Wall -O2 "${SRCDIR}/image_test.cpp" -o ${BINDIR}/image_test
 ${BINDIR}/image_test
 
