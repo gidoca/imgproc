9fca423fdb44492ad95cac3cb16d672aa8a500b7
Gian Calgeer <gidoca@gmail.com>
2024-10-18 20:39:17 +0200

    Move BMP implementation to source file

--- src/colors.h
+++ src/colors.h
@@ -4,7 +4,7 @@
 
 namespace colors {
 namespace u8 {
-static constexpr pixel::PixelU8RGB black = {0, 0, 0};
-static constexpr pixel::PixelU8RGB white = {255, 255, 255};
+constexpr pixel::PixelU8RGB black = {0, 0, 0};
+constexpr pixel::PixelU8RGB white = {255, 255, 255};
 }  // namespace u8
 }  // namespace colors
